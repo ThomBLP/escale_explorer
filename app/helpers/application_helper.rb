44759f108b83948aea543d7edbe496c3e2cbef7f
Thomas Belpomo <thomas.belpomo@hotmail.fr>
2023-11-30 12:12:24 +0100

    svg tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def svg_tag(path, options = {})
+    svg_content = File.open("app/assets/images/#{path.downcase}.svg", "rb") do |file|
+      raw file.read
+    end
+
+    if options[:class].present?
+      svg_content.sub!(/<svg(.*?)>/, "<svg\\1 class='#{options[:class]}'>")
+    end
+
+    raw svg_content
+  end
 end
