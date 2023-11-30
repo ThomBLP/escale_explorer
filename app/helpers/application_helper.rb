bbf65a4bf27a4a74e8b15aba5fef3f99d67ad468
Corentin Simonet <corentin.simonet5@gmail.com>
2023-11-30 12:37:49 +0100

    design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def display_name(name)
+    name = "musées" if name == "musees"
+    return name.capitalize
+  end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -4,11 +4,11 @@ class Category < ApplicationRecord
   validates :name, presence: true
 
   EMOJI_CATEGORIES = {
-    'Monuments' => '🏛️',
-    'Restauration' => '🍔',
-    'Cinéma' => '🎬',
-    'Sport et loisirs' => '⚽',
-    'Balade' => '🚶',
-    'Art' => '🎨'
+    'musees' => '🏛️',
+    'restaurants' => '🍔',
+    'spectacles' => '🎬',
+    'loisirs' => '⚽',
+    'bars' => '🚶',
+    'patrimoine' => '🎨'
   }
 end
