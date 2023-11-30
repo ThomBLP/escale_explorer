6cbb19fde5d78ddccabda15eff607ca6cdd3f59f
Vincent Schmitt <vincentschmitt@free.fr>
2023-11-30 16:12:14 +0100

    home_selection

--- app/javascript/controllers/places_controller.js
+++ app/javascript/controllers/places_controller.js
@@ -4,16 +4,23 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [ "list" ]
   connect() {
+    console.log("connext");
     navigator.geolocation.getCurrentPosition((position) => {
       const latitude  = position.coords.latitude;
       const longitude = position.coords.longitude;
 
-      fetch(`/places?latitude=${latitude}&longitude=${longitude}`,
+      const url = new URL(window.location.href);
+      url.searchParams.set('latitude', latitude);
+      url.searchParams.set('longitude', longitude);
+      console.log(url.href);
+
+      fetch(url.href,
         {
           headers: {"Accept": "text/plain"}
         })
       .then(response => response.text())
       .then((data) => {
+        console.log("data", data);
         this.listTarget.outerHTML = data
       })
     })
