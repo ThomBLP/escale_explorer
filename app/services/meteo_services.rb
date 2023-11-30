2425f2eaac6f7bfae50bc4d6bd7c000dcf3fb688
Thomas Belpomo <thomas.belpomo@hotmail.fr>
2023-11-30 12:07:24 +0100

    fix service

--- app/services/meteo_services.rb
+++ app/services/meteo_services.rb
@@ -1,1 +1,7 @@
-# https://history.openweathermap.org/data/2.5/history/city?lat={lat}&lon={lon}&type=hour&start={start}&end={end}&appid={API key}
+class MeteoServices
+  def initialize
+  end
+  def call
+    url = "https://history.openweathermap.org/data/2.5/history/city?lat={lat}&lon={lon}&type=hour&start={start}&end={end}&appid={API key}"
+  end
+end
