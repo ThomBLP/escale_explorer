5d242bec337518d0795c74540ca3967e2436eaf6
Thomas Belpomo <thomas.belpomo@hotmail.fr>
2023-12-07 16:25:24 +0100

    overlay_fixed

--- app/javascript/controllers/overlay_controller.js
+++ app/javascript/controllers/overlay_controller.js
@@ -1,16 +1,20 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "overlay" ]
+  static targets = [ "overlay", "cross", "bar" ]
   connect() {
-
+    console.log(this.crossTarget);
   }
 
   open() {
     this.element.classList.add("active");
+    this.crossTarget.classList.remove('d-none');
+    this.barTarget.classList.add('d-none');
   }
   close(event) {
     event.stopPropagation();
     this.element.classList.remove("active");
+    this.crossTarget.classList.add('d-none');
+    this.barTarget.classList.remove('d-none');
   }
 }
